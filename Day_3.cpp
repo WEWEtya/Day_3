255eb4f13caaae71c2dd58f6fbb392552b1364c1
WEWEtya <mistersaoman@gmail.com>
2023-05-24 21:17:07 +0300

    Arrays

---
+++ Day_3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+
+	const int size = 5;
+
+	int mass[size] = {0, 5, 6, 3, 6};
+
+	for  (int i = 0; i < size; i++)
+	{
+		cout << mass[i] << endl;
+	}
+
+}
+
