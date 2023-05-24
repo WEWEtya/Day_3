044e8068d560b91a4caa326e3ace8b69a07ebfd6
Viktor <102484631+WEWEtya@users.noreply.github.com>
2023-05-24 21:58:36 +0300

    Add files via upload

---
+++ Day_3.2.cpp
@@ -0,0 +1,30 @@
+// Randomize;
+// Rand; Srand; Srand time null; #include <ctime>
+
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+
+void main()
+{
+	setlocale(LC_ALL, "ukr");
+
+	srand(time(NULL));
+
+	int const size = 10;
+
+	int arr[size];
+
+	for (int i = 0; i < size; i++)
+	{
+		arr[i] = rand() % 1000;
+	}
+
+	for (int i = 0; i < size; i++)
+	{
+		cout << arr[i] << endl;
+	}
+
+}
